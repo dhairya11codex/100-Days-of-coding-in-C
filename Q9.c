b194d8ef26bfd88c5a2b3b54131c24d38e0e7979
dhairya11codex <cricketdhairya11@gmail.com>
2026-09-09 16:40:57 +0530

    Add Q9

--- Q9.c
+++ Q9.c
@@ -1,3 +1,4 @@
+#include <math.h>
 #include <stdio.h>
 int main()
 { 
@@ -9,15 +10,18 @@ int main()
     printf("\nEnter the value of time : ",t);
     scanf("%f",&t);
     printf("\nEnter the value of n : ",n);
-    scanf("%d",&n);
+    scanf("%f",&n);
 
    
     
    
 
     float si = p*r*t/100;
-    float ci = p*(1 + r/n)
+    float ci = p * pow((1 + r/(100*n)),n*t) - p ;
+
     printf("\n The value of simple interest is : %f",si);
+    printf("\nThe value of compound interest is : %f",ci);
+
     return 0;
 
 
